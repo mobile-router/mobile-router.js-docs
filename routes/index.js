8dab543c36206882834065779d3372597797e564
dolymood <dolymood@gmail.com>
2015-04-29 21:43:05 +0800

    sort list

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,30 @@ router.use(function(req, res, next) {
 			}
 		});
 		files.sort();
-		// todo 排序
+
+		var historyIndex = files.indexOf('M.history');
+		var routerIndex = files.indexOf('M.router', historyIndex);
+		var historyLastIndex = findLastIndex(files, 'M.history');
+		var routerLastIndex = findLastIndex(files, 'M.router', historyIndex);
+
+		var betweenHisAndRou = files.slice(historyLastIndex + 1, routerIndex);
+		var afterRou = files.slice(routerLastIndex + 1);
+		betweenHisAndRou.push.apply(betweenHisAndRou, afterRou);
+
+		var args = betweenHisAndRou;
+		args.unshift(0);
+		args.unshift(historyIndex);
+
+		// 移除 history 之后 router 之前的
+		files.splice(historyLastIndex + 1, routerIndex - historyLastIndex - 1);
+		routerLastIndex -= (routerIndex - historyLastIndex - 1);
+		// 移除 router 之后 的
+		files.splice(routerLastIndex + 1, files.length);
+		// 插入之前的
+		files.splice.apply(files, args);
+
+		var dataIndex = files.indexOf('data-');
+		files.push(files.splice(dataIndex, 1)[0]);
 		next();
 	});
 });
@@ -32,3 +55,18 @@ router.get('/:id', function(req, res, next) {
 });
 
 module.exports = router;
+
+function findLastIndex(array, pattern, startIndex) {
+	var index = -1;
+	if (startIndex == undefined) startIndex = 0;
+	var len = pattern.length;
+	array.forEach(function(item, i) {
+		if (i >= startIndex) {
+			var next = item[i + 1];
+			if (item.substr(0, len) === pattern && (!next || next.substr(0, len) !== pattern)) {
+				index = i;
+			}
+		}
+	});
+	return index;
+}
